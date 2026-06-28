c9309316773e42cb8614295366d3e517597eb7f9
Hoang <dovanhoang0@gmail.com>
2026-06-28 22:02:05 +0700

    commit

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ app.add_middleware(
         "http://localhost:5500",
         "http://127.0.0.1:3000",
         "http://localhost:3000",
+        "null",
     ],
     allow_credentials=True,
     allow_methods=["*"],
